61f0a736a10dd9039971804ebb0bee9066134937
Donald Carr <sirspudd@gmail.com>
2014-08-24 17:53:18 -0700

    Publishing WIP
    Change-Id: Ib5d5544036bb30ef93a46868d09d5e229e41811f

--- package.js
+++ package.js
@@ -5,10 +5,19 @@ exports = module.exports = {
 	publish: publish
 };
 
+var tar = require('tar')
+  , fstream = require('fstream')
+  , fs = require('fs');
+
 function install(packageName) {
 	console.log('Installing ' + packageName);
 }
 
-function publish() {
+function publish(path, callback) {
+	var packageFileName = fs.createWriteStream('/tmp/' + 'fuckit.tar');
 	console.log('Publishing your package');
+	fstream.Reader({ path: path, type: 'Directory' })
+		.pipe(tar.Pack({ noProprietary: true }))
+		.pipe(packageFileName)
+		.end(callback);
 }
--- qpm.js
+++ qpm.js
@@ -2,17 +2,22 @@
 
 'use strict';
 
+require('colors');
+
 var program = require('commander'),
 	manifest = require('./package.json'),
 	packageInterface = require('./package.js');
 
 program
   .version(manifest.version)
-  .option('-p, --publish', 'Publish to qpm repo')
+  .option('-p, --publish [path]', 'Publish to qpm repo')
   .option('-i, --install [module]', 'Install [module]')
   .parse(process.argv);
 
-if (program.publish) packageInterface.publish();
+if (program.publish) packageInterface.publish(program.publish, function() {
+ 	console.log('Successfully published module'.green.bold);
+});
+
 if (program.install) packageInterface.install(program.install);
 
 if (process.argv.length < 3) program.help();
