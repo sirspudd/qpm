50c1aaed8cfecf0e47844db637aaca24d43844a7
Donald Carr <sirspudd@gmail.com>
2014-08-25 00:39:43 -0700

    Formatting fix
    Change-Id: Ibc7941a331e738eb211dca0f5fde89394c6e2c77

--- package.js
+++ package.js
@@ -1,48 +1,50 @@
 'use strict';
 
 exports = module.exports = {
-	install: install,
-	publish: publish
+    install: install,
+    publish: publish
 };
 
 require('colors');
 
 var debug = require('debug')('qpm:package'),
     request = require('superagent'),
-	path = require('path'),
+    path = require('path'),
     tar = require('tar-fs'),
-	fs = require('fs');
+    fs = require('fs');
 
 function error() {
-	debug('Error'.red.bold);
+    debug('Error'.red.bold);
 }
 
 function install_success() {
-	debug('Successfully installed module'.green.bold);
+    debug('Successfully installed module'.green.bold);
 }
 
 function publish_success() {
-	debug('Successfully published module'.green.bold);
+    debug('Successfully published module'.green.bold);
 }
 
 function install(packageName) {
-	if (packageName === true) {
-		console.log('Supply a package name to install');
-		process.exit();
-	}
+    if (packageName === true) {
+        console.log('Supply a package name to install');
+        process.exit();
+    }
 
     console.log('Fetching ' + packageName);
 
-    var req = request.get('http://localhost:3000/api/module').query({ name: packageName }).end(function(error, res){
+    var req = request.get('http://localhost:3000/api/module').query({
+        name: packageName
+    }).end(function(error, res) {
         if (error) console.log('Package fetch failed'.red);
     });
 
     var tarStream = tar.extract('quick_modules/' + packageName)
 
     req.pipe(tarStream).on('end', function() {
-	   console.log('Installing ' + packageName);
+        console.log('Installing ' + packageName);
 
-	   tarStream.on('end', install_success).on('error', error);
+        tarStream.on('end', install_success).on('error', error);
     });
 }
 
@@ -54,7 +56,7 @@ function publish(packagePath) {
     var packageInfo = null;
     try {
         packageInfo = JSON.parse(fs.readFileSync(qualifiedPath + '/package.json'));
-    } catch(e) {
+    } catch (e) {
         if ('ENOENT' === e.code) console.log(('No valid package.json found under ' + packagePath).red);
         process.exit(-1);
     }
@@ -67,11 +69,11 @@ function publish(packagePath) {
     console.log('Publishing ' + qualifiedPath);
 
     var tarStream = tar.pack(qualifiedPath, {
-		ignore: function(name) {
-			return name.indexOf('.git') !== -1;
-		}
-	});
+        ignore: function(name) {
+            return name.indexOf('.git') !== -1;
+        }
+    });
 
-	tarStream.pipe(fs.createWriteStream('/tmp/' + path.basename(qualifiedPath) + '.tar'));
-	tarStream.on('end', publish_success).on('error', error);
+    tarStream.pipe(fs.createWriteStream('/tmp/' + path.basename(qualifiedPath) + '.tar'));
+    tarStream.on('end', publish_success).on('error', error);
 }
--- qpm.js
+++ qpm.js
@@ -3,14 +3,13 @@
 'use strict';
 
 var program = require('commander'),
-	manifest = require('./package.json'),
-	packageInterface = require('./package.js');
+    manifest = require('./package.json'),
+    packageInterface = require('./package.js');
 
-program
-  .version(manifest.version)
-  .option('-p, --publish [path]', 'Publish to qpm repo')
-  .option('-i, --install [module]', 'Install [module]')
-  .parse(process.argv);
+program.version(manifest.version)
+    .option('-p, --publish [path]', 'Publish to qpm repo')
+    .option('-i, --install [module]', 'Install [module]')
+    .parse(process.argv);
 
 if (program.publish) packageInterface.publish(program.publish);
 
